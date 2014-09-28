0c1d51f6a7bcf5e022f1c5c03fb1e140fe6bb5ca
Zearin <tgoodchild@gmail.com>
2014-09-28 12:10:14 -0400

    Simplify status check for 3XX responses

--- html5check.py
+++ html5check.py
@@ -179,7 +179,7 @@ redirectCount = 0
 #
 # Make the request
 #
-while (status == 302 or status == 301 or status == 307) and redirectCount < 10:
+while status in (302,301,307) and redirectCount < 10:
   if redirectCount > 0:
     url = response.getheader('Location')
   parsed = urlparse.urlsplit(url)
